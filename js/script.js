9d156cbbbbd058f9dc1bedd054eb9643f061863e
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-03 16:45:14 -0300

    Importados arquivos para criação da pagina

--- js/script.js
+++ js/script.js
@@ -4,82 +4,75 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
-  activaMenuPorSection(home)
-  activaMenuPorSection(services)
-  activaMenuPorSection(about)
-  activaMenuPorSection(contato)
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
-function activaMenuPorSection(section) {
-
+function activateMenuAtCurrentSection(section) {
   const targetLine = scrollY + innerHeight / 2
 
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
   const sectionTop = section.offsetTop
   const sectionHeight = section.offsetHeight
-  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
 
   const sectionEndsAt = sectionTop + sectionHeight
-  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
 
-  const sectionBoundAries = sectionTopReachOrPassedTargetLine 
-  && !sectionEndPassedTargetLine
-
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
 
   const sectionId = section.getAttribute('id')
-  const menuElement = document.
-  querySelector(`.menu a[href*=${sectionId}]`)
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
   menuElement.classList.remove('active')
-  if (sectionBoundAries) {
+  if (sectionBoundaries) {
     menuElement.classList.add('active')
   }
 }
 
-
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navegacao.classList.add("scroll")
+    navigation.classList.add('scroll')
   } else {
-    navegacao.classList.remove("scroll")
+    navigation.classList.remove('scroll')
   }
 }
 
 function showBackToTopButtonOnScroll() {
-  if (scrollY > 400) {
-    backToTopButton.classList.add("show")
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
   } else {
-    backToTopButton.classList.remove("show")
+    backToTopButton.classList.remove('show')
   }
 }
 
-
 function openMenu() {
-  document.body.classList.add('menu-expanded');
+  document.body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded');
+  document.body.classList.remove('menu-expanded')
 }
-
 
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 300,
+  duration: 700
 }).reveal(`
-#home, 
-#home img,
-#home .cards,
-#services,
-#services header,
-#services .card,
-#about,
-#about header,
-#about .content
-`);
-
-
-
-// function sayMyName() {
-//   console.log(nome)
-// }
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
